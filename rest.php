eaa9756ce3adf842f98471baa2aca5541611a845
Octavio Galindo <octavio@euroglas.net>
2020-10-29 17:09:20 -0600

    inicios de rest.php

---
+++ rest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace euroglas\euro-rest;
+
+use altorouter\altorouter;
+use Emarref\Jwt;
+
+class RestServer 
+{
+    private $configPath;
+    private $config;
+    private $router;
+
+    // Este es un arreglo que normalmente debería estar vacio.
+    // se usa para agregar rutas que temporalmente no verifican autorizacion, posiblemente en desarrollo
+    private $testSkipAuth = array(
+    );
+
+    // Estas son las rutas que Nunca requieren autorizacion 
+    // (ojo, son los 'nombres' de las rutas, que se asignan en AltoRouter, no las rutas propiamente dicho)
+    private $realskipAuth = array(
+        'home', // Una pagina HTML a donde 'caemos' cuando no hay una ruta, es la ruta de inicio
+        //'optionsCatchAll',
+        'optionsCatchAllNoSlash',
+    );
+
+
+    function __construct($serverMode = "") {
+        print "In RestServer constructor\n";
+
+        //
+        // Carga la configuración del servidor
+        $ApiName = '';
+        if( !empty($serverMode) )
+        {
+            $this->configPath = "servidor.{$serverMode}.ini";
+        }
+        else
+        {
+            $this->configPath = 'servidor.ini';
+        }
+
+        //print("Cargando configuración desde {$this->configPath}".PHP_EOL);
+        $this->config= parse_ini_file($this->configPath,true);
+
+        // Encabezados básicos
+        header('Access-Control-Allow-Origin: *'); // CORS (Cross-Origin Resource Sharing) desde cualquier origen
+        header('Access-Control-Expose-Headers: content-type, Authorization, ETag, If-None-Match'); // Algunos otros encabezados que necesitamos
+
+        // Inicializa el ruteador
+        $this->router = new AltoRouter();
+
+    }
+
+    // Obten una copia de la configuracion (posiblemente para pasar a otros modulos)
+    public function Config()
+    {
+        return $this->config;
+    }
+}
